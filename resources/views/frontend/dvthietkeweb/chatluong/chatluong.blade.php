c6d9c16fafa009dd19f4fb37c9dc68c01516acb4
QUANGNND <nnduyquang@gmail.com>
2017-02-07 07:48:37 +0700

    OK

---
+++ resources/views/frontend/dvthietkeweb/chatluong/chatluong.blade.php
@@ -0,0 +1,88 @@
+<div class="containerChatLuong">
+    <div class="containSwap col-lg-12">
+        <div class="col-lg-12">
+            <div class="headingText">
+                <h5 class="subHeading">Chất Lượng Đi Đôi Với Sự Hài Lòng</h5>
+                <h3 class="heading">Chất Lượng Website Do <span style="color: #1fb8f1">Smartlink</span> Thiết Kế</h3>
+                <hr class="headingLine">
+            </div>
+        </div>
+        <div class="col-lg-12">
+            <div class="groupChatLuong">
+                <div class="col-lg-3">
+                    <div class="swapOneItemChatLuong">
+                        {{ Html::image('images/temps/chatluong_speed.png','',array('class'=>'imgChatLuong')) }}
+                        <h4>TỐI ƯU TỐC ĐỘ</h4>
+                        <div class="descriptionChatLuong">
+                            Tốc độ load web nhanh, phản hồi ngay lập tức
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="swapOneItemChatLuong">
+                        {{ Html::image('images/temps/chatluong_tichhop.png','',array('class'=>'imgChatLuong')) }}
+                        <h4>TÍCH HỢP DI ĐỘNG</h4>
+                        <div class="descriptionChatLuong">
+                            Hoạt động trên mọi trình duyệt và thiết bị di động
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="swapOneItemChatLuong">
+                        {{ Html::image('images/temps/chatluong_chuanseo.png','',array('class'=>'imgChatLuong')) }}
+                        <h4>CHUẨN SEO</h4>
+                        <div class="descriptionChatLuong">
+                            Giúp quý khách tối ưu hóa chi phí khi quảng cáo và thứ hạng trên Google
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3">
+                    <div class="swapOneItemChatLuong">
+                        {{ Html::image('images/temps/chatluong_thanthien.png','',array('class'=>'imgChatLuong')) }}
+                        <h4>THÂN THIỆN NGƯỜI DÙNG</h4>
+                        <div class="descriptionChatLuong">
+                            Giao diện trực quan dễ sử dụng
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="swapOneItemChatLuong">
+                    {{ Html::image('images/temps/chatluong_huongdan.png','',array('class'=>'imgChatLuong')) }}
+                    <h4>HƯỚNG DẪN CHI TIẾT</h4>
+                    <div class="descriptionChatLuong">
+                        Chúng tôi cung cấp tài liệu hướng dẫn trực quan dễ hiểu
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="swapOneItemChatLuong">
+                    {{ Html::image('images/temps/chatluong_baotri.png','',array('class'=>'imgChatLuong')) }}
+                    <h4>DỄ NÂNG CẤP BẢO TRÌ</h4>
+                    <div class="descriptionChatLuong">
+                        Dễ dàng thêm chức năng mới khi quý khách yêu cầu
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="swapOneItemChatLuong">
+                    {{ Html::image('images/temps/chatluong_baohanh.png','',array('class'=>'imgChatLuong')) }}
+                    <h4>BẢO HÀNH TRỌN ĐỜI</h4>
+                    <div class="descriptionChatLuong">
+                        Chúng tôi bảo hành trang web quý khách trọn đời
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="swapOneItemChatLuong">
+                    {{ Html::image('images/temps/chatluong_hailong.png','',array('class'=>'imgChatLuong')) }}
+                    <h4>HÀI LÒNG</h4>
+                    <div class="descriptionChatLuong">
+                        Chúng tôi đảm bảo sự hài lòng của quý khách khi thiết kế website tại Smartlinks
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
--- resources/views/frontend/dvthietkeweb/thietkewebsite.blade.php
+++ resources/views/frontend/dvthietkeweb/thietkewebsite.blade.php
@@ -2,4 +2,5 @@
 @section('container')
     @include('frontend.dvthietkeweb.header.header')
     @include('frontend.dvthietkeweb.taisao.taisao')
+    @include('frontend.dvthietkeweb.chatluong.chatluong')
 @stop
